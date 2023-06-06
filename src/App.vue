d6cec8358cd76b8eaea60b9834b48fbc2d6feb97
DIFENG1997 <hugodufourrire@outlook.com>
2023-06-06 13:48:38 +0800

    msg

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+
+    <Element-View>
+
+    </Element-View>
+
+  </div>
+  <!--   <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+  </div> -->
+</template>
+
+<script>
+import ElementView from "./views/element/ElementView.vue";
+export default {
+  components: {
+    ElementView
+  }
+
+}
+
+
+</script>
+
